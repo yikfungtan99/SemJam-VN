eb6fff7b0e36e6904999029ea06d7b83daea2338
yikfungtan99 <50047634+yikfungtan99@users.noreply.github.com>
2019-10-05 02:01:15 +0800

    fix invi button

--- Assets/ButtonManager.cs
+++ Assets/ButtonManager.cs
@@ -7,20 +7,31 @@ public class ButtonManager : MonoBehaviour
 {
     public GameManager gm;
 
-    public RectTransform button1;
-    public RectTransform button2;
+    public GameObject button1;
+    public GameObject button2;
+
+    private RectTransform buttonRectTransform1;
+    private RectTransform buttonRectTransform2;
 
     public Vector2[] button1Pos;
-    public Vector2[] button1Size;
+    public float[] button1Width;
+
+    public Vector2[] button2Pos;
+    public float[] button2Width;
 
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        buttonRectTransform1 = button1.GetComponent<RectTransform>();
+    }
 
     public void ChangePos()
     {
         Debug.Log(gm.x);
 
-        button1.localPosition = button1Pos[gm.x];
-        button1.localScale = button1Size[gm.x];
+        buttonRectTransform1.localPosition = button1Pos[gm.x];
 
+        buttonRectTransform1.sizeDelta = new Vector2(button1Width[gm.x], 64);
     }
 }
